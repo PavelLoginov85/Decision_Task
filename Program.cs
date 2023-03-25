eb64620d80053ac40312e3afb895c493f68a26ea
Pavel <pahnologia@yandex.ru>
2023-03-25 13:19:24 +0300

    Delete all litter

--- Program.cs
+++ Program.cs
@@ -1,36 +1,7 @@
-﻿// // сделать массив из 12 эл-ов в диапазоне (-9 , 9) и найти сумму + и -
-// int [] massive = MakeArray(12);
-// Console.WriteLine($"[{String.Join(",", massive)}]");
-// int plusSum = 0;
-// int minsSum = 0;
-// foreach (int el in massive){
-//     plusSum += el > 0 ? el : 0;
-//     minsSum += el < 0? el : 0;
-// }
-
-// Console.WriteLine($"Сумма всех + {plusSum},сумма всех отрицательных чисел {minsSum}");
-
-// /*-------------------------------- Method---------------------------------*/
-
-// int [] MakeArray(int quantity)
-// {
-//     int[] array = new int[quantity];
-//     for (int i = 0; i < quantity; i++)
-//     {
-//         array [i] = new Random().Next(-9, 10);
-
-//     }
-//     return array;
-// }
-
-
-
-
-
-// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
 // которая покажет количество чётных чисел в массиве.
 
-// [345, 897, 568, 234] -> 2
+
 
 // int[] vassive = GetArray(10, 99, 1000);
 // int chert = 0;
@@ -58,8 +29,6 @@
 // Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
 //стоящих на нечётных индексах.
 
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
 
 
 //int[] SumElement(int size, int minValue, int maxValue);
@@ -91,45 +60,44 @@
 
 // Задача 3: Задайте массив вещественных чисел. Найдите разницу между 
 // максимальным и минимальным элементов массива.
-// [3, 7.4, 22.3, 2, 78] -> 76
 
 
 
-Console.Write("Укажите размер массива: ");
-int n = Math.Abs(int.Parse(Console.ReadLine()!));
-double[] s = diffBetween(n);
+// Console.Write("Укажите размер массива: ");
+// int n = Math.Abs(int.Parse(Console.ReadLine()!));
+// double[] s = diffBetween(n);
 
- double min = s[0];
- double max = s[0];
-
-
-if (n > 20) {
-    Console.WriteLine("Имейте совесть!, принимаются значения не более 20 ");
-}
-
-else{
-Console.WriteLine($"[{String.Join("; ", s)}]");
-
-foreach (double el in s)
-{
-    min = el < min? min = el : min;
-    max = el > max? max = el : max;
-}
+//  double min = s[0];
+//  double max = s[0];
 
 
-double difference = Math.Round(max - min, 2);
-Console.WriteLine($"Разница между Мах и Мин числом в указанном диапазоне равна:{difference}");
+// if (n > 20) {
+//     Console.WriteLine("Имейте совесть!, принимаются значения не более 20 ");
+// }
+
+// else{
+// Console.WriteLine($"[{String.Join("; ", s)}]");
+
+// foreach (double el in s)
+// {
+//     min = el < min? min = el : min;
+//     max = el > max? max = el : max;
+// }
 
 
-}
-/*-------------------------- Method -------------------------------*/
-double[] diffBetween(int size)
-{
+// double difference = Math.Round(max - min, 2);
+// Console.WriteLine($"Разница между Мах и Мин числом в указанном диапазоне равна:{difference}");
 
-    double[] array = new double[size];
-    Random rand = new Random();
-    for (int i = 0; i < size; i++)
-        array[i] = Math.Round((rand.NextDouble()) * 100, 2);
-    return array;
-}
+
+// }
+// /*---------------------------------------- Method ----------------------------------------------*/
+// double[] diffBetween(int size)
+// {
+
+//     double[] array = new double[size];
+//     Random rand = new Random();
+//     for (int i = 0; i < size; i++)
+//         array[i] = Math.Round((rand.NextDouble()) * 100, 2);
+//     return array;
+// }
 
